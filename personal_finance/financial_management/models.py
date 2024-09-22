c16cc69826b3028895017187262b095cdb48585e
Izan Mata <matadiazizan@gmail.com>
2024-09-22 21:50:45 +0200

    init app

--- personal_finance/financial_management/models.py
+++ personal_finance/financial_management/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    pass
+class Account(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='savings_plans')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Transaction(models.Model):
 
@@ -16,10 +19,23 @@ class Transaction(models.Model):
         ANNUAL = 'annual', 'Annual'
 
     type = models.CharField(max_length=7, choices=Type.choices)
-    date = models.DateField(default=timezone.now)
+    datetime = models.DateField(default=timezone.now)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255)
     periodicity = models.CharField(max_length=7, choices=Periodicity.choices)
 
     def __str__(self) -> str:
         return f"{self.type.capitalize()}: {self.amount} ({self.description})"
+    
+
+class SavingsPlan(models.Model):
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='savings_plans')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    target_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expected_date = models.DateField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.name
