29ec075e55039e554864e55f6028ee1dbca555a0
Izan Mata <matadiazizan@gmail.com>
2024-09-23 14:28:00 +0200

    categories endpoint

--- personal_finance/personal_finance/repositories/category_repository.py
+++ personal_finance/personal_finance/repositories/category_repository.py
@@ -1,10 +1,8 @@
 from django.db.models.query import QuerySet
 from financial_management.models import Category
+from django.core.exceptions import ObjectDoesNotExist
 
 class CategoryRepository():
 
     def get_all(self) -> QuerySet[Category]:
         return Category.objects.all()
-        
-    def get_by_id(self, id : int):
-        pass
