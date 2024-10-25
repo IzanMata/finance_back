948cb92a56227db47d4ee0e6a3e288cde4832ed0
Izan Mata <matadiazizan@gmail.com>
2024-10-25 19:55:26 +0200

    New ide

--- personal_finance/personal_finance/serializers/transaction_serializer.py
+++ personal_finance/personal_finance/serializers/transaction_serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from financial_management.models import Transaction
+from personal_finance.serializers.category_serializer import CategorySerializer
 
 class TransactionSerializer(serializers.ModelSerializer):
+    
+    category = CategorySerializer(read_only=True)
     
     class Meta:
         model = Transaction
